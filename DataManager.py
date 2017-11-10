a6c0407f37d12d33c99d62b48aa423ca003b9f08
Matan <matanbn22@gmail.com>
2017-11-10 16:19:59 +0200

    Fixed previous inefficiency with dicts and fixed starting values with 1 instead of 0 when adding values to dicts

--- DataManager.py
+++ DataManager.py
@@ -1,3 +1,6 @@
+import time
+
+
 def parse_pos_reading(lines):
     content = [["^^^^^/Start"] + line.split(" ")[0:-1] for line in lines]
     data = [word for line in content for word in line]
@@ -20,30 +23,33 @@ def read_file(file_name, parse_func):
 
 def write_file(file_name, content, parse_func):
     file = open(file_name, 'w')
-    file.write(parse_pos_writing(content))
+    file.write(parse_func(content))
     file.close()
+
+
 
 
 def count_labels(labels, order):
     labels_count = dict()
     labels_set = set()
-    for j in range(1, order + 1):
-        for i in range(0, len(labels)):
-            labels_set.add(labels[i])
-            order_gram = " ".join(labels[i:i + j])
-            if (order_gram not in labels_count.keys()):
-                labels_count[order_gram] = 0
+    for i in range(1, order + 1):
+        for j in range(0, len(labels)):
+            labels_set.add(labels[j])
+            order_gram = " ".join(labels[j:i + j])
+            if order_gram not in labels_count:
+                labels_count[order_gram] = 1
             else:
                 labels_count[order_gram] += 1
     return labels_count, labels_set
 
 
+
 def label_word_pairs(words, labels):
-    pairs_count = {}
+    pairs_count = dict()
     for word, label in zip(words, labels):
         pair = word + " " + label
-        if pair not in pairs_count.keys():
-            pairs_count[pair] = 0
+        if pair not in pairs_count:
+            pairs_count[pair] = 1
         else:
             pairs_count[pair] += 1
     return pairs_count
@@ -64,7 +70,6 @@ class ProbabilityContainer:
     def get_q_probs(self, labels):
         raise NotImplementedError
 
-
 words_and_labels = read_file("data/ass1-tagger-train", parse_pos_reading)
 words = [data[0] for data in words_and_labels]
 labels = [data[1] for data in words_and_labels]
@@ -74,3 +79,4 @@ write_file("q.mle", labels_count, parse_pos_writing)
 
 pairs_count = label_word_pairs(words, labels)
 write_file("e.mle", pairs_count, parse_pos_writing)
+
