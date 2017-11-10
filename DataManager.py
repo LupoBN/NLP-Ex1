cbec4452eef02811ab56df1df64e304b9d8d01af
Matan <matanbn22@gmail.com>
2017-11-10 15:08:13 +0200

    Seperated reading, writing and parsing data to different functions, added Probability Class instead of DataManger which will give the q and e values.

--- DataManager.py
+++ DataManager.py
@@ -1,60 +1,76 @@
-# TODO: Split might loss some numbers between them are 5/100 (5 divided by 100).
-class DataManager:
-    def __init__(self):
+def parse_pos_reading(lines):
+    content = [["^^^^^/Start"] + line.split(" ")[0:-1] for line in lines]
+    data = [word for line in content for word in line]
+    return [word.rsplit("/", 1) for word in data]
 
-    def read_file(self, file_name):
-        file = open(file_name, 'r')
-        lines = file.readlines()
-        lines = [line.split(" ")[0:-1] for line in lines]
-        data = [word for line in lines for word in line]
-        words_and_labels = [["".join(word.split("/")[0:-1]), word.split("/")[-1]] for word in data]
-        words = [data[0] for data in words_and_labels]
-        labels = [data[1] for data in words_and_labels]
 
-        file.close()
+def parse_pos_writing(counts):
+    my_str = ""
+    for key in counts.keys():
+        my_str += key + '\t' + str(counts[key]) + '\n'
+    return my_str
 
-        assert len(words) == len(labels)
-        return words_and_labels, words, labels
 
-    def count_labels(labels, order):
-        labels_count = dict()
-        labels_set = set()
-        for j in range(1, order + 1):
-            for i in range(0, len(labels)):
-                labels_set.add(labels[i])
-                order_gram = " ".join(labels[i:i + j])
-                if (order_gram not in labels_count.keys()):
-                    labels_count[order_gram] = 0
-                else:
-                    labels_count[order_gram] += 1
-        return labels_count, labels_set
+def read_file(file_name, parse_func):
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    file.close()
+    return parse_func(lines)
 
-    def label_word_pairs(words, labels):
-        pairs_count = {}
-        for word, label in zip(words, labels):
-            pair = word + " " + label
-            if pair not in pairs_count.keys():
-                pairs_count[pair] = 0
+
+def write_file(file_name, content, parse_func):
+    file = open(file_name, 'w')
+    file.write(parse_pos_writing(content))
+    file.close()
+
+
+def count_labels(labels, order):
+    labels_count = dict()
+    labels_set = set()
+    for j in range(1, order + 1):
+        for i in range(0, len(labels)):
+            labels_set.add(labels[i])
+            order_gram = " ".join(labels[i:i + j])
+            if (order_gram not in labels_count.keys()):
+                labels_count[order_gram] = 0
             else:
-                pairs_count[pair] += 1
+                labels_count[order_gram] += 1
+    return labels_count, labels_set
 
-        return pairs_count
 
-    def write_statistics(statistics, file_name):
-        file = open(file_name, 'w')
-        for key in statistics.keys():
-            file.write(key + '\t' + str(statistics[key]) + '\n')
-        file.close()
+def label_word_pairs(words, labels):
+    pairs_count = {}
+    for word, label in zip(words, labels):
+        pair = word + " " + label
+        if pair not in pairs_count.keys():
+            pairs_count[pair] = 0
+        else:
+            pairs_count[pair] += 1
+    return pairs_count
 
-    def get_e_probs(words, labels):
+
+# TODO: Split might loss some numbers between them are 5/100 (5 divided by 100).
+class ProbabilityContainer:
+    def __init__(self):
+        self._q = dict()
+        self._e = dict()
+
+    def calculate_probs(self, labels_count, label_word_count):
+        pass
+
+    def get_e_probs(self, words, labels):
         raise NotImplementedError
 
-    def get_q_probs(labels):
-        raise NotADirectoryError
+    def get_q_probs(self, labels):
+        raise NotImplementedError
 
 
-words_and_labels, words, labels = DataManager.read_file("data/ass1-tagger-train")
-labels_count, labels_set = DataManager.count_labels(labels, 3)
-DataManager.write_statistics(labels_count, "q.mle")
-pairs_count = DataManager.label_word_pairs(words, labels)
-DataManager.write_statistics(pairs_count, 'e.mle')
+words_and_labels = read_file("data/ass1-tagger-train", parse_pos_reading)
+words = [data[0] for data in words_and_labels]
+labels = [data[1] for data in words_and_labels]
+
+labels_count, labels_set = count_labels(labels, 3)
+write_file("q.mle", labels_count, parse_pos_writing)
+
+pairs_count = label_word_pairs(words, labels)
+write_file("e.mle", pairs_count, parse_pos_writing)
