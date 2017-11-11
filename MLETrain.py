5a3d694adc860eb2a057d0fb388ca92d403ca1e7
shauli1321 <shauli321@gmail.com>
2017-11-11 21:36:11 +0200

    added creating the possible labels file

--- MLETrain.py
+++ MLETrain.py
@@ -11,3 +11,6 @@ if __name__ == '__main__':
 
     pairs_count = label_word_pairs(words, labels)
     write_file(sys.argv[3], pairs_count, parse_pos_writing)
+
+    possible_labels = parse_possible_labels(words_and_labels)
+    write_file("data/possible_labels.txt", possible_labels, parse_possible_labels_writing)
