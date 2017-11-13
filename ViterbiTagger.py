cd9b66423de51d01015911579126c180899677bc
shauli1321 <shauli321@gmail.com>
2017-11-13 22:50:38 +0200

    Viterbi seems to be working now, needs more tests

--- ViterbiTagger.py
+++ ViterbiTagger.py
@@ -1,4 +1,7 @@
 import random
+import DataManager
+import numpy as np
+import math
 
 class DummyProbsProvider():
     def __init__(self):
@@ -14,11 +17,11 @@ class DummyProbsProvider():
 
 
 class ViterbiTagger:
-    def __init__(self, labels_set, possible_labels):
-        self.labels_set = list(labels_set)
-        self.possible_labels  = possible_labels
-        self.probs = DummyProbsProvider()
-
+    def __init__(self, probs_provider):
+        self.labels_set = list(probs_provider.get_label_set())
+        #self.possible_labels  = probs_provider.get_label_set()
+        self.probs = probs_provider
+        #self.labels_set = ["Start", "DT", "NN"]
     def predict_tags(self, words):
         """
 
@@ -28,34 +31,50 @@ class ViterbiTagger:
 
         """initialization """
         V, bp = [], []
-        n = len(words)
+        n = len(words)+1
+        l = len(self.labels_set)
         for i in range(n):
             V.append([[0. for j in range(len(self.labels_set))] for k in range(len(self.labels_set))])
             bp.append([[0. for j in range(len(self.labels_set))] for k in range(len(self.labels_set))])
 
-
-        start_key = self.labels_set.index("START")
+        V = np.zeros((n, l, l))
+        bp = np.zeros((n, l, l), dtype=int)
+        print "Shape is ", V.shape
+        start_key = self.labels_set.index("Start")
+        print "start_key is ", start_key
         V[0][start_key][start_key] = 1.
+        V[1][start_key][start_key] = 1.
         bp[0][start_key][start_key] = start_key
+        bp[1][start_key][start_key] = start_key
 
         """ compute the prob. of a sequence of length i that ends with the labels t, r"""
-
+        s=""
+        epsilon = 1e-8
         for i in range(1, n): #skip two first start symbols.
-            word = words[i]
+            if i < 2:
+                word = "^^^^^"
+            else:
+                word = words[i-1]
+            """
             if word in self.possible_labels:
                 word_possible_labels = self.possible_labels[word]
             else:
                 word_possible_labels = self.labels_set
-
+            """
             for t_index, t in enumerate(self.labels_set):
-                for r_index, r in word_possible_labels:
-                   max_val, max_t_prime = -1, None
+                for r_index, r in enumerate(self.labels_set):
+
+                   max_val, max_t_prime = -float("inf"), None
                    for t_prime_index, t_prime in enumerate(self.labels_set):
                        V_prev_t_t_prime = V[i-1][t_prime_index][t_index]
-                       q = self.probs.get_q(r, t_prime, t)
-                       r = self.probs.get_e(word, r)
-                       if V_prev_t_t_prime * q * r > max_val:
-                           max_val = V_prev_t_t_prime * q * r
+
+                       q = self.probs.get_q_prob(r, t, t_prime)
+                       e = self.probs.get_e_prob(word, r)
+
+                       score = q * e * V_prev_t_t_prime
+
+                       if score > max_val:
+                           max_val = score
                            max_t_prime = t_prime_index
 
                    V[i][t_index][r_index] = max_val
@@ -63,29 +82,20 @@ class ViterbiTagger:
         """
         calcualte max on t, r of V[n-1]
         """
-        V_last = V[n-1]
-        max_val = -1
-        y_n, y_n_minus1 = None, None
+        V_last = V[-1]
 
 
-        for i, t in enumerate(self.labels_set):
-            for j, r in enumerate(self.labels_set):
-                if V_last[i][j] > max_val:
-                    max_val = V_last[i][j]
-                    y_n, y_n_minus1 = i, j
+        y_n_minus1, y_n = np.unravel_index(np.argmax(V_last),V_last.shape)
 
-        y = [None] * n
-        y[-1], y[-2] = y_n, y_n_minus1
+        y = [0]*n
+        y[-2], y[-1] = y_n_minus1, y_n
         for i in range(n-3, 0, -1):
             y[i] = bp[i+2][y[i+1]][y[i+2]]
+        print "y: ", y
+        preds = [self.labels_set[val] for val in y]
+        return preds[2:]
 
-        predictions  = []
-        for i, l in enumerate(y[1:]):
-            predictions.append(self.labels_set[l])
-
-        return predictions
-
-
+"""
 labels_set = {"START", "NN", "NP", "V"}
 words = "START hello how are you ? I am alright".split(" ")
 
@@ -99,3 +109,14 @@ for line in f.readlines():
 
 gt = ViterbiTagger(labels_set, possible_labels)
 print gt.predict_tags(words)
+"""
+if __name__ == '__main__':
+    probability_provider = DataManager.ProbabilityContainer("e.mle", "q.mle" )
+    words = "^^^^^ Yes of course".split(" ")
+    vt = ViterbiTagger(probability_provider)
+    preds =  vt.predict_tags(words)
+    words = words[1:]
+    s = ""
+    for i, word in enumerate(words):
+        s+=word+ "("+preds[i]+") "
+    print s
