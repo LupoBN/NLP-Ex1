f0fbe184b72d666fa03a179c650b5f59bacbdd5c
shauli1321 <shauli321@gmail.com>
2017-11-16 12:01:07 +0200

    minor changes

--- TrainSolver.py
+++ TrainSolver.py
@@ -2,7 +2,7 @@ import DataManager
 import ExtractFeatures
 import ConvertFeatures
 import sklearn
-from sklearn.datasets import load_svmlight_file
+
 
 class Trainer():
 
@@ -11,7 +11,5 @@ class Trainer():
 
 
 if __name__ == '__main__':
- f = open("train_features", "r")
- lines = f.readlines()
- X_train, y_train = load_svmlight_file("feature_vecs_file")
- print (X_train).getnnz()
+ pass
+
